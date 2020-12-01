1d66e07538792799c2f12c6b1d381e2c1374d623
wlhuang <wlhuang@linkdome.cn>
2020-12-01 20:23:28 +0800

    add infer test folder, test result speed and accuracy are lower than DBFace

--- tools/infer.py
+++ tools/infer.py
@@ -2,6 +2,8 @@ import argparse
 import cv2
 import numpy as np
 import torch
+import os
+import time
 
 from vedacore.image import imread, imwrite
 from vedacore.misc import Config, color_val, load_weights
@@ -24,6 +26,7 @@ def prepare(cfg):
         device = torch.cuda.current_device()
     else:
         device = 'cpu'
+    # device = 'cpu'
     engine = build_engine(cfg.infer_engine)
 
     engine.model.to(device)
@@ -63,6 +66,18 @@ def plot_result(result, imgfp, class_names, outfp='out.jpg'):
                     cv2.FONT_HERSHEY_COMPLEX, font_scale, text_color)
     imwrite(img, outfp)
 
+image_ext = ['.jpg', '.jpeg', '.webp', '.bmp', '.png']
+video_ext = ['mp4', 'mov', 'avi', 'mkv']
+def get_image_list(path):
+    image_names = []
+    for maindir, subdir, file_name_list in os.walk(path):
+        for filename in file_name_list:
+            apath = os.path.join(maindir, filename)
+            ext = os.path.splitext(apath)[1]
+            if ext in image_ext:
+                image_names.append(apath)
+    return image_names
+
 
 def main():
 
@@ -72,6 +87,7 @@ def main():
     class_names = cfg.class_names
 
     engine, data_pipeline, device = prepare(cfg)
+
 
     data = dict(img_info=dict(filename=imgname), img_prefix=None)
 
@@ -84,9 +100,65 @@ def main():
         # just get the actual data from DataContainer
         data['img_metas'] = data['img_metas'][0].data
         data['img'] = data['img'][0].data
+
+
     result = engine.infer(data['img'], data['img_metas'])[0]
+
     plot_result(result, imgname, class_names)
 
 
+
+
+def main_floder():
+    args = parse_args()
+    cfg = Config.fromfile(args.config)
+    imgPath = args.imgname
+    class_names = cfg.class_names
+
+    engine, data_pipeline, device = prepare(cfg)
+
+    files = get_image_list(imgPath)
+    files.sort()
+    total_time=0
+    for imgname in files:
+        st = time.time()  # 返回当前的
+
+        data = dict(img_info=dict(filename=imgname), img_prefix=None)
+
+        data = data_pipeline(data)
+        data = collate([data], samples_per_gpu=1)
+        if device != 'cpu':
+            # scatter to specified GPU
+            data = scatter(data, [device])[0]
+        else:
+            # just get the actual data from DataContainer
+            data['img_metas'] = data['img_metas'][0].data
+            data['img'] = data['img'][0].data
+
+        ed = time.time()  # 返回当前的
+        print("tinaface 图片预处理时间:%.3f秒" % (ed - st))
+
+        result = engine.infer(data['img'], data['img_metas'])[0]
+        ed1 = time.time()
+        print("tinaface运行时间:%.3f秒" % (ed1 - ed))
+        total_time += (ed1-st)
+
+        plot_result(result, imgname, class_names,"./result/"+imgname.split("/")[-1])
+        ed2 = time.time()
+        print("tinaface运行+save时间:%.3f秒" % (ed2 - ed1))
+
+        print("tinaface 预处理＋运行+save时间:%.3f秒" % (ed2 - st))
+    t1 = total_time / len(files)
+    fps = 1.0 / t1
+    print("total time:%.3f秒" % t1)
+    print("total time:%.3f秒" % fps)
+
 if __name__ == '__main__':
-    main()
+    args = parse_args()
+    imgPath = args.imgname
+
+    ext = os.path.splitext(imgPath)[1]
+    if ext in image_ext:
+        main()
+    else:
+        main_floder()
